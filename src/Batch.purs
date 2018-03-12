61467938386039a740d0a18bb096ef9c2b554c40
Philippe Vinchon <pvinchon@gilt.com>
2018-03-12 17:35:13 +0000

    PureScript mapping for aws-sdk-js 2.207.0

--- src/Batch.purs
+++ src/Batch.purs
@@ -20,87 +20,117 @@ import Data.StrMap as StrMap
 import AWS.Request as Request
 import AWS.Request.Types as Types
 
-serviceName = "Batch" :: String
-
 
 -- | <p>Cancels a job in an AWS Batch job queue. Jobs that are in the <code>SUBMITTED</code>, <code>PENDING</code>, or <code>RUNNABLE</code> state are cancelled. Jobs that have progressed to <code>STARTING</code> or <code>RUNNING</code> are not cancelled (but the API operation still succeeds, even if no job is cancelled); these jobs must be terminated with the <a>TerminateJob</a> operation.</p>
 cancelJob :: forall eff. CancelJobRequest -> Aff (exception :: EXCEPTION | eff) CancelJobResponse
-cancelJob = Request.request serviceName "cancelJob" 
+cancelJob = Request.request service method  where
+    service = Request.ServiceName "Batch"
+    method = Request.MethodName "cancelJob"
 
 
 -- | <p>Creates an AWS Batch compute environment. You can create <code>MANAGED</code> or <code>UNMANAGED</code> compute environments.</p> <p>In a managed compute environment, AWS Batch manages the compute resources within the environment, based on the compute resources that you specify. Instances launched into a managed compute environment use a recent, approved version of the Amazon ECS-optimized AMI. You can choose to use Amazon EC2 On-Demand Instances in your managed compute environment, or you can use Amazon EC2 Spot Instances that only launch when the Spot bid price is below a specified percentage of the On-Demand price.</p> <p>In an unmanaged compute environment, you can manage your own compute resources. This provides more compute resource configuration options, such as using a custom AMI, but you must ensure that your AMI meets the Amazon ECS container instance AMI specification. For more information, see <a href="http://docs.aws.amazon.com/AmazonECS/latest/developerguide/container_instance_AMIs.html">Container Instance AMIs</a> in the <i>Amazon Elastic Container Service Developer Guide</i>. After you have created your unmanaged compute environment, you can use the <a>DescribeComputeEnvironments</a> operation to find the Amazon ECS cluster that is associated with it and then manually launch your container instances into that Amazon ECS cluster. For more information, see <a href="http://docs.aws.amazon.com/AmazonECS/latest/developerguide/launch_container_instance.html">Launching an Amazon ECS Container Instance</a> in the <i>Amazon Elastic Container Service Developer Guide</i>.</p>
 createComputeEnvironment :: forall eff. CreateComputeEnvironmentRequest -> Aff (exception :: EXCEPTION | eff) CreateComputeEnvironmentResponse
-createComputeEnvironment = Request.request serviceName "createComputeEnvironment" 
+createComputeEnvironment = Request.request service method  where
+    service = Request.ServiceName "Batch"
+    method = Request.MethodName "createComputeEnvironment"
 
 
 -- | <p>Creates an AWS Batch job queue. When you create a job queue, you associate one or more compute environments to the queue and assign an order of preference for the compute environments.</p> <p>You also set a priority to the job queue that determines the order in which the AWS Batch scheduler places jobs onto its associated compute environments. For example, if a compute environment is associated with more than one job queue, the job queue with a higher priority is given preference for scheduling jobs to that compute environment.</p>
 createJobQueue :: forall eff. CreateJobQueueRequest -> Aff (exception :: EXCEPTION | eff) CreateJobQueueResponse
-createJobQueue = Request.request serviceName "createJobQueue" 
+createJobQueue = Request.request service method  where
+    service = Request.ServiceName "Batch"
+    method = Request.MethodName "createJobQueue"
 
 
 -- | <p>Deletes an AWS Batch compute environment.</p> <p>Before you can delete a compute environment, you must set its state to <code>DISABLED</code> with the <a>UpdateComputeEnvironment</a> API operation and disassociate it from any job queues with the <a>UpdateJobQueue</a> API operation.</p>
 deleteComputeEnvironment :: forall eff. DeleteComputeEnvironmentRequest -> Aff (exception :: EXCEPTION | eff) DeleteComputeEnvironmentResponse
-deleteComputeEnvironment = Request.request serviceName "deleteComputeEnvironment" 
+deleteComputeEnvironment = Request.request service method  where
+    service = Request.ServiceName "Batch"
+    method = Request.MethodName "deleteComputeEnvironment"
 
 
 -- | <p>Deletes the specified job queue. You must first disable submissions for a queue with the <a>UpdateJobQueue</a> operation. All jobs in the queue are terminated when you delete a job queue.</p> <p>It is not necessary to disassociate compute environments from a queue before submitting a <code>DeleteJobQueue</code> request. </p>
 deleteJobQueue :: forall eff. DeleteJobQueueRequest -> Aff (exception :: EXCEPTION | eff) DeleteJobQueueResponse
-deleteJobQueue = Request.request serviceName "deleteJobQueue" 
+deleteJobQueue = Request.request service method  where
+    service = Request.ServiceName "Batch"
+    method = Request.MethodName "deleteJobQueue"
 
 
 -- | <p>Deregisters an AWS Batch job definition.</p>
 deregisterJobDefinition :: forall eff. DeregisterJobDefinitionRequest -> Aff (exception :: EXCEPTION | eff) DeregisterJobDefinitionResponse
-deregisterJobDefinition = Request.request serviceName "deregisterJobDefinition" 
+deregisterJobDefinition = Request.request service method  where
+    service = Request.ServiceName "Batch"
+    method = Request.MethodName "deregisterJobDefinition"
 
 
 -- | <p>Describes one or more of your compute environments.</p> <p>If you are using an unmanaged compute environment, you can use the <code>DescribeComputeEnvironment</code> operation to determine the <code>ecsClusterArn</code> that you should launch your Amazon ECS container instances into.</p>
 describeComputeEnvironments :: forall eff. DescribeComputeEnvironmentsRequest -> Aff (exception :: EXCEPTION | eff) DescribeComputeEnvironmentsResponse
-describeComputeEnvironments = Request.request serviceName "describeComputeEnvironments" 
+describeComputeEnvironments = Request.request service method  where
+    service = Request.ServiceName "Batch"
+    method = Request.MethodName "describeComputeEnvironments"
 
 
 -- | <p>Describes a list of job definitions. You can specify a <code>status</code> (such as <code>ACTIVE</code>) to only return job definitions that match that status.</p>
 describeJobDefinitions :: forall eff. DescribeJobDefinitionsRequest -> Aff (exception :: EXCEPTION | eff) DescribeJobDefinitionsResponse
-describeJobDefinitions = Request.request serviceName "describeJobDefinitions" 
+describeJobDefinitions = Request.request service method  where
+    service = Request.ServiceName "Batch"
+    method = Request.MethodName "describeJobDefinitions"
 
 
 -- | <p>Describes one or more of your job queues.</p>
 describeJobQueues :: forall eff. DescribeJobQueuesRequest -> Aff (exception :: EXCEPTION | eff) DescribeJobQueuesResponse
-describeJobQueues = Request.request serviceName "describeJobQueues" 
+describeJobQueues = Request.request service method  where
+    service = Request.ServiceName "Batch"
+    method = Request.MethodName "describeJobQueues"
 
 
 -- | <p>Describes a list of AWS Batch jobs.</p>
 describeJobs :: forall eff. DescribeJobsRequest -> Aff (exception :: EXCEPTION | eff) DescribeJobsResponse
-describeJobs = Request.request serviceName "describeJobs" 
+describeJobs = Request.request service method  where
+    service = Request.ServiceName "Batch"
+    method = Request.MethodName "describeJobs"
 
 
 -- | <p>Returns a list of task jobs for a specified job queue. You can filter the results by job status with the <code>jobStatus</code> parameter. If you do not specify a status, only <code>RUNNING</code> jobs are returned.</p>
 listJobs :: forall eff. ListJobsRequest -> Aff (exception :: EXCEPTION | eff) ListJobsResponse
-listJobs = Request.request serviceName "listJobs" 
+listJobs = Request.request service method  where
+    service = Request.ServiceName "Batch"
+    method = Request.MethodName "listJobs"
 
 
 -- | <p>Registers an AWS Batch job definition. </p>
 registerJobDefinition :: forall eff. RegisterJobDefinitionRequest -> Aff (exception :: EXCEPTION | eff) RegisterJobDefinitionResponse
-registerJobDefinition = Request.request serviceName "registerJobDefinition" 
+registerJobDefinition = Request.request service method  where
+    service = Request.ServiceName "Batch"
+    method = Request.MethodName "registerJobDefinition"
 
 
 -- | <p>Submits an AWS Batch job from a job definition. Parameters specified during <a>SubmitJob</a> override parameters defined in the job definition. </p>
 submitJob :: forall eff. SubmitJobRequest -> Aff (exception :: EXCEPTION | eff) SubmitJobResponse
-submitJob = Request.request serviceName "submitJob" 
+submitJob = Request.request service method  where
+    service = Request.ServiceName "Batch"
+    method = Request.MethodName "submitJob"
 
 
 -- | <p>Terminates a job in a job queue. Jobs that are in the <code>STARTING</code> or <code>RUNNING</code> state are terminated, which causes them to transition to <code>FAILED</code>. Jobs that have not progressed to the <code>STARTING</code> state are cancelled.</p>
 terminateJob :: forall eff. TerminateJobRequest -> Aff (exception :: EXCEPTION | eff) TerminateJobResponse
-terminateJob = Request.request serviceName "terminateJob" 
+terminateJob = Request.request service method  where
+    service = Request.ServiceName "Batch"
+    method = Request.MethodName "terminateJob"
 
 
 -- | <p>Updates an AWS Batch compute environment.</p>
 updateComputeEnvironment :: forall eff. UpdateComputeEnvironmentRequest -> Aff (exception :: EXCEPTION | eff) UpdateComputeEnvironmentResponse
-updateComputeEnvironment = Request.request serviceName "updateComputeEnvironment" 
+updateComputeEnvironment = Request.request service method  where
+    service = Request.ServiceName "Batch"
+    method = Request.MethodName "updateComputeEnvironment"
 
 
 -- | <p>Updates a job queue.</p>
 updateJobQueue :: forall eff. UpdateJobQueueRequest -> Aff (exception :: EXCEPTION | eff) UpdateJobQueueResponse
-updateJobQueue = Request.request serviceName "updateJobQueue" 
+updateJobQueue = Request.request service method  where
+    service = Request.ServiceName "Batch"
+    method = Request.MethodName "updateJobQueue"
 
 
 newtype ArrayJobDependency = ArrayJobDependency String
